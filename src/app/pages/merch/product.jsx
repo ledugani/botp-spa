835744c9e7a79c5827b58c1bfcd00b846f8a5002
Tom Dugan <thomas@aadwebb.com>
2020-12-29 15:14:26 -0600

    Revert "where am i"
    This reverts commit 21d65072ed61e83e3120ff43a72e21dcdb6b84e4.

--- src/app/pages/merch/product.jsx
+++ src/app/pages/merch/product.jsx
@@ -1,82 +1,15 @@
 import React, { useState } from 'react';
-import Options from './options';
-import './styles.css';
 
-export default function Product({ name, sizes, qtys, options }) {
-	const [selectedOption, setSelectedOption] = useState(options[0]);
-
+export default function Product({ name, image, sizes, styles, colors }) {
+	const [selectedColor, setSelectedColor] = useState(colors[0]);
 	return (
 		<div className="product">
 			<div className="name">{name}</div>
+			{colors.map((color) => (
+				<div className='color' onClick={() => setSelectedColor(color)}>{color.name}</div>
+			))}
 
-			<Options
-				sizes={sizes}
-				selectedSize={selectedOption}
-				setSelectedSize={setSelectedOption}
-			/>
-{/*
-			<QtyOptions
-				Qtys={qtys}
-				selectedQty={selectedQty}
-				setSelectedQty={setSelectedQty}
-			/>
-
-			<ColorOptions
-				colors={colors}
-				selectedColor={selectedColor}
-				setSelectedColor={setSelectedColor}
-			/> */}
-
-			<div className="price">
-				Price: ${selectedOption.price.toFixed(2)}
-				{/* limits characters to a number of spaces - 2 for dollars (.00) */}
-			</div>
-
-			<img width={400} src={selectedOption.image} alt={name} />
+			<img width={100} src={selectedColor.image} alt={name} />
 		</div>
 	);
 }
-
-// import React, { useState } from 'react';
-// import Options from './options';
-// import './styles.css';
-
-// export default function Product({ name, styles, sizes, qtys }) {
-// 	const [selectedSize, setSelectedSize] = useState(sizes[0]);
-// 	const [selectedStyle, setSelectedStyle] = useState(styles[0]);
-// 	const [selectedQuantity, setSelectedQuantity] = useState(qtys[0]);
-
-// 	return (
-// 		<div className="product">
-// 			<div className="name">{name}</div>
-
-// 			<Options
-// 				label='Available Sizes:'
-// 				options={sizes}
-// 				selectedOption={selectedSize}
-// 				setSelectedOption={setSelectedSize}
-// 			/>
-
-// 			<Options
-// 				label='Quantity:'
-// 				options={qtys}
-// 				selectedOption={selectedQuantity}
-// 				setSelectedOption={setSelectedQuantity}
-// 			/>
-
-// 			<Options
-// 				label='Available Styles:'
-// 				options={styles}
-// 				selectedOption={selectedStyle}
-// 				setSelectedOption={setSelectedStyle}
-// 			/>
-
-// 			<div className="price">
-// 				Price: ${(selectedStyle.price * selectedSize.price).toFixed(2)}
-// 				{/* limits characters to a number of spaces - 2 for dollars (.00) */}
-// 			</div>
-
-// 			<img width={400} src={selectedStyle.image} alt={name} />
-// 		</div>
-// 	);
-// }
--- src/app/seo/index.jsx
+++ src/app/seo/index.jsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 
 export default function Seo({ title, description }) {
 	document.querySelector('title').innerHTML = title;
