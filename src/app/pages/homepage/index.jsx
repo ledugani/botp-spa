2ae97dde24bda217ced779aa3aaa51ff04efc842
Tom Dugan <thomas@aadwebb.com>
2020-12-29 09:14:35 -0600

    added merch page

--- src/app/pages/homepage/index.jsx
+++ src/app/pages/homepage/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import Seo from '../../seo';
 
-export default function Homepage({ counter }) {
+export default function Homepage({  }) {
 	return (
 		<>
 			<Seo title="Home" />
 			<h1 className="heading">Welcome to Bottom of the Pile</h1>
-			{counter}
 			<p>Lorem ipsum dolor, site amet consectetur adipisicing.</p>
 
 		</>
---
+++ src/app/pages/merch/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Seo from '../../seo';
+
+export default function MerchPage({  }) {
+	return (
+		<>
+			<Seo title="Merch" />
+			<h1>Merch</h1>
+		</>
+	);
+}
