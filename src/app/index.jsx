51994651eb60cc8431ad71dfb1f4b04441dcf3c1
Tom Dugan <thomas@aadwebb.com>
2020-12-28 20:01:26 -0600

    added route for about page

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Layout from "./layout";
 import Homepage from "./pages/homepage";
+import AboutPage from "./pages/about";
 
 export default function App() {
 	return (
 		<Layout>
-			<Homepage />
+			<BrowserRouter>
+				<Switch>
+					<Route exact path='/'>
+						<Homepage />
+					</Route>
+
+					<Route path='/about'>
+						<AboutPage />
+					</Route>
+				</Switch>
+			</BrowserRouter>
 		</Layout>
 		);
 }
