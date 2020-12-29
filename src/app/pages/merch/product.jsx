909c23fe04b4fe9480f056538d70ec1cb91387b1
Tom Dugan <thomas@aadwebb.com>
2020-12-29 15:48:02 -0600

    functioning color picker buttons

--- src/app/pages/merch/product.jsx
+++ src/app/pages/merch/product.jsx
@@ -1,15 +1,29 @@
 import React, { useState } from 'react';
+import './styles.css';
 
 export default function Product({ name, image, sizes, styles, colors }) {
 	const [selectedColor, setSelectedColor] = useState(colors[0]);
 	return (
 		<div className="product">
 			<div className="name">{name}</div>
-			{colors.map((color) => (
-				<div className='color' onClick={() => setSelectedColor(color)}>{color.name}</div>
-			))}
+			<div className="colors">
+				available in:
+				{colors.map((color) => (
+					<span
+						className={`color ${
+							selectedColor === color ? 'selected' : null
+						}`}
+						onClick={() => setSelectedColor(color)}>
+						{color.name}
+					</span>
+				))}
+			</div>
 
-			<img width={100} src={selectedColor.image} alt={name} />
+			<img
+				width={400}
+				src={selectedColor.image}
+				alt={name}
+			/>
 		</div>
 	);
 }
