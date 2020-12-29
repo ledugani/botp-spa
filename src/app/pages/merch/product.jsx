2f3d918befbf36279b77fb506cef4a7a69d8b8db
Tom Dugan <thomas@aadwebb.com>
2020-12-29 13:53:08 -0600

    needs to be DRY'd

--- src/app/pages/merch/product.jsx
+++ src/app/pages/merch/product.jsx
@@ -1,15 +1,42 @@
 import React, { useState } from 'react';
+import ColorOptions from './color-options';
+import QtyOptions from './quantity-options';
+import SizeOptions from './size-options';
+import './styles.css';
 
-export default function Product({ name, image, sizes, styles, colors }) {
+export default function Product({ name, sizes, qtys, colors }) {
 	const [selectedColor, setSelectedColor] = useState(colors[0]);
+	const [selectedQty, setSelectedQty] = useState(qtys[0]);
+	const [selectedSize, setSelectedSize] = useState(sizes[0]);
+
 	return (
 		<div className="product">
 			<div className="name">{name}</div>
-			{colors.map((color) => (
-				<div className='color' onClick={() => setSelectedColor(color)}>{color.name}</div>
-			))}
 
-			<img width={100} src={selectedColor.image} alt={name} />
+			<SizeOptions
+				sizes={sizes}
+				selectedSize={selectedSize}
+				setSelectedSize={setSelectedSize}
+			/>
+
+			<QtyOptions
+				Qtys={qtys}
+				selectedQty={selectedQty}
+				setSelectedQty={setSelectedQty}
+			/>
+
+			<ColorOptions
+				colors={colors}
+				selectedColor={selectedColor}
+				setSelectedColor={setSelectedColor}
+			/>
+
+			<div className="price">
+				Price: ${selectedStyle.price.toFixed(2)}
+				{/* limits characters to a number of spaces - 2 for dollars (.00) */}
+			</div>
+
+			<img width={400} src={selectedColor.image} alt={name} />
 		</div>
 	);
 }
