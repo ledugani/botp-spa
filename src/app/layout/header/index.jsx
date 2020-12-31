8ac2a8e1d7ba5550c3d7e3861ee42b98161cf6a7
Tom Dugan <thomas@aadwebb.com>
2020-12-30 18:51:18 -0600

    fix?

--- src/app/layout/header/index.jsx
+++ src/app/layout/header/index.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { getCartItems } from '../../../modules/cart';
 import './styles.css';
 
-export default function Header({  }) {
+export default function Header({ }) {
 	return <div className='__dml header'>
 		<ul>
 			<li>
