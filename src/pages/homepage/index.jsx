9e036ab7088eb11343373f26827fed7687742800
Tom Dugan <thomas@aadwebb.com>
2021-01-08 19:28:48 -0600

    added logo, changed bg color

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import Seo from '../../app/seo';
 import Tracks from './tracks/tracks';
+import logo from '../img/spotify-botp-variant-cream.png';
 
-//import './styles.css';
+import './styles.css';
 
 export default function Homepage() {
 
 	return (
 		<>
 			<Seo title="Home" />
+
+			<img src={logo} alt="botp logo" className="botp-logo" />
+
 			<h1 className="heading">Welcome to Bottom of the Pile</h1>
 			<p>
 				Bottom of the Pile is a music discovery platform,
@@ -17,6 +21,8 @@ export default function Homepage() {
 
 			{/* Best New Tracks */}
 			<div className="best-new-tracks">
+				<h3>Best New Tracks</h3>
+
 				<Tracks />
 			</div>
 		</>
