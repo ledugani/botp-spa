e442f06bf57ed1ff363c8ad87800e811c93a1489
Tom Dugan <thomas@aadwebb.com>
2021-01-11 11:23:24 -0600

    conditional render card class for long strings

--- src/pages/homepage/tracks/track/index.jsx
+++ src/pages/homepage/tracks/track/index.jsx
@@ -6,7 +6,9 @@ export default function Track({ trackDetails }) {
 
 	const track = trackDetails.track;
 
-	console.log(track);
+	const trackCharLength = track.album.name + track.name + track.artists[0].name + track.album.name + track.album.release_date;
+
+	// console.log(track);
 
 	return (
 		<>
@@ -17,11 +19,10 @@ export default function Track({ trackDetails }) {
 					className="album-cover"
 					width={150}
 				/>
-				<div className="song-details">
+				<div className={trackCharLength.length > 100 ? "song-details-lg" : "song-details"}>
 					<h3 className="track-title">{track.name}</h3>
 					<h4 className="track-artist">{track.artists[0].name}</h4>
 					<h4 className="track-album">{track.album.name}</h4>
-
 				</div>
 				<h5 className="track-release">{track.album.release_date}</h5>
 			</div>
