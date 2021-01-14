da7a6e83b4bb0c26380b25a1f10b2a62b3607bd0
Tom Dugan <thomas@aadwebb.com>
2021-01-14 17:18:26 -0600

    sign up rendering

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import CartContext from './cart/context';
-import useCart from '../hooks/use-cart';
+import SignUp from './auth/signup';
+import { Container } from 'react-bootstrap';
 
-import Layout from './layout';
-import Homepage from '../pages/homepage';
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import CartContext from './cart/context';
+// import useCart from '../hooks/use-cart';
+// import Layout from './layout';
+// import Homepage from '../pages/homepage';
 // import AboutPage from '../pages/about';
 // import MerchPage from '../pages/merch';
 // import CartPage from '../pages/cart';
@@ -13,28 +15,42 @@ import './styles.css';
 
 export default function App() {
 	return (
-		<CartContext.Provider value={useCart([])}>
-			<BrowserRouter>
-				<Layout>
-					<Switch>
-						<Route exact path='/'>
-							<Homepage />
-						</Route>
-{/*
-						<Route path='/about'>
-							<AboutPage />
-						</Route>
+		<Container
+			className='d-flex align-items-center justify-content-center'
+			style={{ minHeight: '100vh' }}
+		>
+			<div
+				className='w-100'
+				style={{ maxWidth: '400px' }}
+			>
+				<SignUp />
+			</div>
+		</Container>
+	)
 
-						<Route path='/merch'>
-							<MerchPage />
-						</Route>
+	// return (
+	// 	<CartContext.Provider value={useCart([])}>
+	// 		<BrowserRouter>
+	// 			<Layout>
+	// 				<Switch>
+	// 					<Route exact path='/'>
+	// 						<Homepage />
+	// 					</Route>
 
-						<Route path='/cart'>
-							<CartPage />
-						</Route> */}
-					</Switch>
-				</Layout>
-			</BrowserRouter>
-		</CartContext.Provider>
-	);
+	// 					<Route path='/about'>
+	// 						<AboutPage />
+	// 					</Route>
+
+	// 					<Route path='/merch'>
+	// 						<MerchPage />
+	// 					</Route>
+
+	// 					<Route path='/cart'>
+	// 						<CartPage />
+	// 					</Route>
+	// 				</Switch>
+	// 			</Layout>
+	// 		</BrowserRouter>
+	// 	</CartContext.Provider>
+	// );
 }
