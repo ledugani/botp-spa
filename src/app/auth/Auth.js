fce6c9ff3279b085daa2fc8c786e972162f8c48e
Tom Dugan <thomas@aadwebb.com>
2021-01-14 17:54:43 -0600

    sign up working

--- src/app/auth/Auth.js
+++ src/app/auth/Auth.js
@@ -8,11 +8,13 @@ export function useAuth() {
 }
 
 export function AuthProvider({ children }) {
-	const [currentUser, setCurrentUser] = useState(null);
+	const [currentUser, setCurrentUser] = useState();
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		const unsubscribe = auth.onAuthStateChanged(user => {
-			setCurrentUser(user)
+			setCurrentUser(user);
+			setLoading(false);
 		});
 
 		return unsubscribe;
@@ -29,7 +31,7 @@ export function AuthProvider({ children }) {
 
 	return (
 		<AuthContext.Provider value={ value }>
-			{children}
+			{ !loading && children }
 		</AuthContext.Provider>
 	)
 }
