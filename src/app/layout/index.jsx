71e8eba05d8c7462af8852231570e5b75ba6ca2b
Tom Dugan <thomas@aadwebb.com>
2021-01-17 14:57:24 -0600

    conditionally rendering navbar based on user auth

--- src/app/layout/index.jsx
+++ src/app/layout/index.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Header from './header';
+import PrivateHeader from './header/private';
 import Footer from './footer';
+import { useAuth } from '../auth/Auth';
 import './styles.css';
 
 export default function Layout({ children }) {
+	const { currentUser } = useAuth();
+
 	return <div className="__dml container">
-		<Header />
+		{
+			currentUser !== null
+			? <PrivateHeader />
+			: <Header />
+		}
 		<div className="page-content">
 			{children}
 		</div>
